7c1415bfffea0094b3bfe0d96b4a4bd9b31a7b2e
bmait101 <Bryan.Maitland@usda.gov>
2024-11-13 14:39:58 -0700

    document RMRSMT103 data

---
+++ R/RMRS_MT103.R
@@ -0,0 +1,15 @@
+#' Daily stream temperatures data
+#'
+#' Daily stream temperature data from the US Forest Service Rocky
+#' Mountain Research Station, Water and Watersheds Program.
+#'
+#' @source Dan Isaak, US Forest Service Rocky Mountain Research Station
+#' @format Data frame with columns
+#' \describe{
+#' \item{SampleDate}{Date measurement was taken.}
+#' \item{site}{Site ID.}
+#' \item{temperture}{Temperature in degrees Celsius.}
+#' }
+#' @examples
+#'   RMRS_MT103
+"RMRS_MT103"
