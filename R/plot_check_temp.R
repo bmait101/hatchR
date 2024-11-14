b7659c5ae494dcde39da51225eb72e3bb384b132
bmait101 <Bryan.Maitland@usda.gov>
2024-11-14 10:03:45 -0700

    updates to plot_temp_check()

--- R/plot_check_temp.R
+++ R/plot_check_temp.R
@@ -1,59 +1,59 @@
-#' Plot imported data
+#' Plot imported data to check temperature values
 #'
-#' @param df data.frame with dates and temperature
+#' The `plot_check_temp` function is used to plot imported data to check temperature values.
+#' The function takes a data frame with dates and temperature values,
+#' and plots the temperature values over time. The function also allows users
+#' to specify the minimum and maximum temperature values to be plotted.
+#'
+#' @param data data.frame with dates and temperature
 #' @param dates date column
 #' @param temperature temperature column
 #' @param temp_min min temp on plot
 #' @param temp_max max temp on plot
 #'
 #' @return
+#' A object of class "gg" and "ggplot" that can be printed to the console or saved as an image.
+#'
 #' @export
 #'
 #' @examples
-plot_check_temp <- function(df,
+#' library(hatchR)
+#' plot_check_temp(data = RMRS_MT103, dates = SampleDate, temperature = temperature)
+plot_check_temp <- function(data,
                             dates,
                             temperature,
                             temp_min = 0,
                             temp_max = 25
                             ){
 
-  check_dates <- df |>
-    pull({{dates}}) |>
-    is.character()
-
-  if(check == TRUE){
-    stop("Date column is a character vector; covert to timepoint object with lubridate (e.g. `ymd()`.")
+  check_dates <- data |> dplyr::pull({{dates}}) |>is.character()
+  if(check_dates == TRUE){
+    stop(
+      "Date column is not Date or Date-time class; convert to date or date-time (e.g. `lubridate::ymd()`.",
+      call. = FALSE
+      )
     }
 
-  # object holder for dates character check
-  d <- df |> dplyr::pull({{dates}})
+  # d <- data |> dplyr::pull({{dates}})
+  # if(class({{d}})[1] != "Date" | class({{d}})[1] != "POSIXct"){
+  #   stop("Date column is not Date or Date-time class; convert to date or date-time (e.g. `lubridate::ymd()`.")
+  # }
 
-  # if dates is string, convert to timepoint via lubridate
-  if(is.character(d)){
-    p <- df  |>
-      dplyr::mutate(dates = lubridate::ymd_hms({{dates}})) |>
-      ggplot2::ggplot(ggplot2::aes(x = dates, y = {{temperature}})) +
-      ggplot2::geom_point(size =0.5) +
-      ggplot2::geom_line(linewidth =0.5) +
-      ggplot2::geom_hline(
-        yintercept = c(temp_min), linetype = "dashed", color = "dodgerblue") +
-      ggplot2::geom_hline(
-        yintercept = c(temp_max), linetype = "dashed", color = "red") +
-      ggplot2::labs(x = "Date", y = "Temperature") +
-      ggplot2::theme_classic()
+  p <- data |>
+    ggplot2::ggplot(ggplot2::aes(x = {{dates}}, y = {{temperature}})) +
+    ggplot2::geom_point(size = 0.5) +
+    ggplot2::geom_line(linewidth = 0.5) +
+    ggplot2::geom_hline(
+      yintercept = c(temp_min), linetype = "dashed", color = "dodgerblue") +
+    ggplot2::geom_hline(
+      yintercept = c(temp_max), linetype = "dashed", color = "red") +
+    ggplot2::labs(x = "Date", y = "Temperature") +
+    ggplot2::theme_classic()
 
-  } else{
-    p <- df |>
-      ggplot2::ggplot(ggplot2::aes(x = {{dates}}, y = {{temperature}})) +
-      ggplot2::geom_point(size =0.5) +
-      ggplot2::geom_line(linewidth =0.5) +
-      ggplot2::geom_hline(
-        yintercept = c(temp_min), linetype = "dashed", color = "dodgerblue") +
-      ggplot2::geom_hline(
-        yintercept = c(temp_max), linetype = "dashed", color = "red") +
-      ggplot2::labs(x = "Date", y = "Temperature") +
-      ggplot2::theme_classic()
-
-  }
   return(p)
 }
+
+# TO DO:
+# - Add a title to the plot
+# - Add a subtitle to the plot
+# - add x and y axis scales that depend on the data
