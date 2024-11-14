bd11917744b4f17871e66e3a66a8053a1ee13962
bmait101 <Bryan.Maitland@usda.gov>
2024-11-14 10:55:58 -0700

    document model_table

---
+++ R/model_table.R
@@ -0,0 +1,16 @@
+#' Model parameterizations for salmonid species to predict phenology
+#'
+#' Description to come...
+#'
+#' @source To come...
+#' @format Data frame
+#' \describe{
+#' \item{author}{Short article citation}
+#' \item{species}{Species name}
+#' \item{model}{Model number from B&M}
+#' \item{dev.type}{Hatch or emergence}
+#' \item{func}{character string desciption of the function}
+#' }
+#' @examples
+#'   model_table
+"model_table"
