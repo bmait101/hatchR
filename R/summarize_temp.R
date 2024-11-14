a245bb3c6e8e299c1d3e9498518e8d4e29235775
bmait101 <Bryan.Maitland@usda.gov>
2024-11-14 10:36:10 -0700

    fix summarize_temp() and documnent

--- R/summarize_temp.R
+++ R/summarize_temp.R
@@ -3,30 +3,35 @@
 #' The `summarize_temp` function is used to summarize sub-daily temperature
 #' measurements to obtain mean daily temperature.
 #'
-#' @param data
-#' @param dates
-#' @param temperature
+#' @param data data.frame with dates and temperature
+#' @param dates date column
+#' @param temperature temperature column
 #'
 #' @return
+#' A data frame with summarized daily temperature values.
+#'
 #' @export
 #'
 #' @examples
-summarize_temp <- function(data, dates, temperature){
+#' library(hatchR)
+#' summarize_temp(data = RMRS_MT103, dates = SampleDate, temperature = temperature)
+summarize_temp <- function(data,
+                           dates,
+                           temperature
+                           ){
 
-  ## check to make sure data are in correct format (check.1 is for datetime object, check.2 is for Date object)
-  check <- data %>%
-    pull({{dates}}) %>%
-    is.character()
+  check_dates <- data |> dplyr::pull({{dates}}) |> is.character()
+  if(check_dates == TRUE){
+    stop(
+      "Date column is not Date or Date-time class; convert to date or date-time (e.g. `lubridate::ymd()`.",
+      call. = FALSE
+    )
+  }
 
-
-  if(check == TRUE){stop("Your dates are formatted as a character they need to be formatted as a timepoint object with lubridate (e.g. ymd()" )}
-
-
-  ### if data pass checks, run function
-  sum_dat <- data %>%
-    mutate(ag_date = date({{dates}})) %>% # mutate date-time to date only
-    group_by(ag_date) %>% # group by a unique day
-    summarise(daily_temp = mean({{temperature}})) # summarize to daily mean temp
+  sum_dat <- data  |>
+    dplyr::mutate(date = lubridate::date({{dates}}))  |>
+    dplyr::group_by(date)  |>
+    dplyr::summarise(daily_temp = mean({{temperature}}))
 
   return(sum_dat)
 }
