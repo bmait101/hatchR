8b5bcf662daef303d25e4ebaffc02ef185b20f14
bmait101 <Bryan.Maitland@usda.gov>
2024-11-15 09:40:36 -0700

    document woody_island

---
+++ R/woody_island.R
@@ -0,0 +1,17 @@
+#' Temperature Data from Lake Iliamna, Alaska
+#'
+#' @description
+#' Temperature Data from Lake Iliamna on Woody Island, Alaska
+#'
+#' @source To come...
+#' @format Data frame
+#' \describe{
+#' \item{author}{Short article citation}
+#' \item{species}{Species name}
+#' \item{model}{Model number from B&M}
+#' \item{dev.type}{Hatch or emergence}
+#' \item{func}{character string desciption of the function}
+#' }
+#' @examples
+#'   woody_island
+"woody_island"
