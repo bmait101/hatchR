2e844691eb97776ebb4328955f67082742bbaa96
bmait101 <Bryan.Maitland@usda.gov>
2024-11-15 09:44:18 -0700

    add woody_island to globalVariables

--- R/hatchR-package.R
+++ R/hatchR-package.R
@@ -1,7 +1,7 @@
 #' @keywords internal
 "_PACKAGE"
 
-utils::globalVariables(c("model_table"))
+utils::globalVariables(c("model_table","woody_island"))
 
 ## usethis namespace: start
 #' @importFrom utils globalVariables
