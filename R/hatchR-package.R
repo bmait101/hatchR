3ff9f6fbedf639f64a166ea12b01ce493db041f9
bmait101 <Bryan.Maitland@usda.gov>
2024-11-18 14:31:23 -0700

    add stats functions

--- R/hatchR-package.R
+++ R/hatchR-package.R
@@ -5,6 +5,9 @@ utils::globalVariables(c("model_table", "woody_island"))
 
 ## usethis namespace: start
 #' @importFrom rlang .data
+#' @importFrom stats coef
+#' @importFrom stats lm
+#' @importFrom stats nls
 #' @importFrom utils globalVariables
 ## usethis namespace: end
 NULL
