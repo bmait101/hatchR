31488a4541475826a02055892c1ddc90698acdf6
bmait101 <Bryan.Maitland@usda.gov>
2024-11-18 16:10:24 -0700

    add tests for plot_check_temp()

---
+++ tests/testthat/test-plot_check_temp.R
@@ -0,0 +1,13 @@
+test_that("plot_check_temp() returns list", {
+  p <- plot_check_temp(RMRS_MT103, SampleDate, temperature)
+  expect_type(p, "list")
+  expect_s3_class(p, "gg")
+  plot_names <- names(p)
+  expect_identical(plot_names, c("data", "layers", "scales", "guides",
+                                 "mapping", "theme", "coordinates",
+                                 "facet", "plot_env", "layout", "labels"))
+  expect_s3_class(p$data, "data.frame")
+  plot_geoms <- sapply(p$layers, function(x) class(x$geom)[1])
+  expect_identical(plot_geoms, c("GeomPoint", "GeomLine", "GeomHline", "GeomHline"))
+  expect_invisible(plot(p))
+})
