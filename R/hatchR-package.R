cb26d301e14afb9e593be053963e9a71d8ce131c
bmait101 <Bryan.Maitland@usda.gov>
2024-11-19 16:22:05 -0700

    update NAMESPACE

--- R/hatchR-package.R
+++ R/hatchR-package.R
@@ -4,12 +4,7 @@
 utils::globalVariables(c("model_table", "woody_island"))
 
 ## usethis namespace: start
-#' @importFrom modelr add_predictions
 #' @importFrom rlang .data
-#' @importFrom stats coef
-#' @importFrom stats lm
-#' @importFrom stats nls
-#' @importFrom stats predict
 #' @importFrom utils globalVariables
 ## usethis namespace: end
 NULL
