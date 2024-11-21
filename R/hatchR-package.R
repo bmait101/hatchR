4e73758688a72ffcc72a419ccb26eeb800eaf88e
bmait101 <Bryan.Maitland@usda.gov>
2024-11-21 09:11:23 -0700

    set up lifecycles

--- R/hatchR-package.R
+++ R/hatchR-package.R
@@ -4,6 +4,7 @@
 utils::globalVariables(c("model_table", "woody_island"))
 
 ## usethis namespace: start
+#' @importFrom lifecycle deprecated
 #' @importFrom rlang .data
 #' @importFrom utils globalVariables
 ## usethis namespace: end
