36ff8ee1beb847d30611a05b2f77ba8fa4c9ce36
bmait101 <Bryan.Maitland@usda.gov>
2024-12-03 16:07:27 -0700

    fix bugs

--- R/fit_model.R
+++ R/fit_model.R
@@ -14,9 +14,8 @@
 #' specific discussion regarding model 2 and the development of the
 #' effective value approach.
 #'
-#' @param df Data.frame with new data
-#' @param temp Column with temperature measurement
-#' @param days Column with day of temperature measurement
+#' @param temp Numeric vector of temperatures
+#' @param days Numeric vector of days to hatch or emerge
 #' @param species Character string of species name (e.g., "sockeye")
 #' @param dev.type Character string of development type: "hatch" or "emerge"
 #'
@@ -31,10 +30,10 @@
 #' temperature <- c(2, 5, 8, 11, 14)
 #' # vector of days to hatch
 #' days_to_hatch <- c(194, 87, 54, 35, 28)
-#' quinn_bt_hatch <- tibble::tibble(temperature, days_to_hatch)
-#' bt_hatch_mod <- fit_model(df = quinn_bt_hatch, temp = temperature,
+#' bt_hatch_mod <- fit_model(temp = temperature,
 #' days = days_to_hatch, species = "sockeye", dev.type = "hatch")
-fit_model <- function(df, temp, days, species = NULL, dev.type = NULL) {
+fit_model <- function(temp, days, species = NULL, dev.type = NULL) {
+  df <- tibble::tibble(x = temp, y = days)
 
   # check if species is NULL
   if (is.null(species)) {
@@ -52,19 +51,22 @@ fit_model <- function(df, temp, days, species = NULL, dev.type = NULL) {
     ))
   }
 
+
   # fit linear model to log data
-  m1 <- stats::lm(log(days) ~ log(temp), data = df)
-  summary(m1)
+  m1 <- stats::lm(log(y) ~ x, data = df)
+  # summary(m1)
 
   # estimate starting values for nls
-  m1_a <- exp(stats::coef(m1)[1]) # exponentiate the intercept
-  m1_b <- stats::coef(m1)[2]
+  st <- list(
+    a = exp(stats::coef(m1)[1]),
+    b = stats::coef(m1)[2]
+    )
 
   # fit model 2 from Beacham & Murray (1990) to data
-  m2 <- stats::nls(days ~ a / (temp - b), data = df, start = list(a = m1_a, b = m1_b))
-  summary(m2)
+  m2 <- stats::nls(y ~ a / (x - b), data = df, start = st)
+  # summary(m2)
 
-  # check coefficients
+  # get coefficients
   m2_a <- log(stats::coef(m2)[1])
   m2_b <- stats::coef(m2)[2]
 
@@ -75,23 +77,22 @@ fit_model <- function(df, temp, days, species = NULL, dev.type = NULL) {
     dev.type = dev.type,
     func = func
     )
-  # mod <- parse(text = mod)  # no longer parsing until predict_phenology()
 
   # plot predictions and data --------------------------
 
-  grid <- data.frame(temp = seq(min(temp), max(temp), 0.1))
+  grid <- data.frame(x = seq(min(df$x), max(df$x), 0.1))
   grid$pred <- stats::predict(m2, newdata = grid)
   p_pred <- df |>
-    ggplot2::ggplot(ggplot2::aes(x = temp, y = days)) +
+    ggplot2::ggplot(ggplot2::aes(x = .data$x, y = .data$y)) +
     ggplot2::geom_point() +
-    ggplot2::geom_line(data = grid, ggplot2::aes(x = .data$temp, y = .data$pred), col = "blue") +
+    ggplot2::geom_line(data = grid, ggplot2::aes(x = .data$x, y = .data$pred), col = "blue") +
     ggplot2::theme_classic()
   p_pred
 
   # model diagnostics --------------------------------
 
   # Predicted values
-  y <- temp
+  y <- df$y
   y_pred <- stats::predict(m2)
 
   # Calculate residuals
