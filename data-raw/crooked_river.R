c010f22e0fa4777a442b2a1a57e99ec22c87cc6a
bmait101 <Bryan.Maitland@usda.gov>
2024-12-05 11:16:55 -0700

    omit lib call

--- data-raw/crooked_river.R
+++ data-raw/crooked_river.R
@@ -1,5 +1,3 @@
-library(readr)
-
-crooked_river <- read_csv("data-raw/crooked_river.csv")
+crooked_river <- readr::read_csv("data-raw/crooked_river.csv")
 
 usethis::use_data(crooked_river, overwrite = TRUE)
