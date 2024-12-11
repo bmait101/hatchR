6e728dff218b2df5dbe7389847ec4474862db77e
Morgan.Sparks <morgan.sparks@morgans-mbp.boise.rmrs.fs.fed.us>
2024-12-11 14:28:26 -0700

    add hatchR_logo in inst/misc to hold script and images for input/ouput for hatchR logo

---
+++ inst/misc/hexlogos/hatchR_logo.R
@@ -0,0 +1,12 @@
+library(hexSticker)
+
+# current logo
+sticker(here::here("inst/misc/hexlogos/hatchR_FINAL.tif"), package="hatchR", p_size=20, s_x=1, s_y=.75, s_width=.6,
+        h_fill="white", h_color = "black", p_color = "black",
+        filename=here::here("inst/misc/hexlogos/hatchR_logo.png"))
+
+
+# FS colors
+sticker("~/Downloads/hatchR_FINAL.tif", package="hatchR", p_size=20, s_x=1, s_y=.75, s_width=.6,
+        h_fill="#01502f", h_color = "#ffd330", p_color = "#ffd330",
+        filename="~/Downloads/hatchR_hexsticker.png")
